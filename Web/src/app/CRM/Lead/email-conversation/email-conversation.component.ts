d76db4b2086515c39b133de12d80336d3e4c11a8
DurgaPrasadRaju <durgaprasad.j@avowstech.com>
2024-06-12 17:52:58 +0800

    Structure added for CRM

---
+++ Web/src/app/CRM/Lead/email-conversation/email-conversation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-conversation',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-conversation.component.html',
+  styleUrl: './email-conversation.component.css'
+})
+export class EmailConversationComponent {
+
+}
--- Web/src/app/app.routes.ts
+++ Web/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { EmailConversationComponent } from './CRM/Lead/email-conversation/email-conversation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: 'email-conversation',component:EmailConversationComponent}];
