b1d7cbdf440680b6a8c48803ed673475f4772537
DurgaPrasadRaju <durgaprasad.j@avowstech.com>
2024-06-14 11:49:20 +0800

    added code for email-conversation

--- Web/src/app/CRM/Lead/email-conversation/email-conversation.component.ts
+++ Web/src/app/CRM/Lead/email-conversation/email-conversation.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { NgxEditorModule,Toolbar } from 'ngx-editor';
+
+
 
 @Component({
   selector: 'app-email-conversation',
   standalone: true,
-  imports: [],
+  imports: [NgxEditorModule],
   templateUrl: './email-conversation.component.html',
   styleUrl: './email-conversation.component.css'
 })
-export class EmailConversationComponent {
 
+
+export class EmailConversationComponent {
+  editor: any;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+ 
 }
+
